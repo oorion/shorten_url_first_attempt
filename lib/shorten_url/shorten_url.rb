54766fe6b631e3c19fd8ddc4e4d659880ef33f1f
Orion Osborn <orion.osborn@gmail.com>
2015-03-28 19:55:45 -0600

    wrap JetFuel class in module

--- lib/shorten_url/shorten_url.rb
+++ lib/shorten_url/shorten_url.rb
@@ -1,10 +1,12 @@
-class JetFuel
-  def initialize(protocol_and_host)
-    @protocol_and_host = protocol_and_host
-  end
+module ShortenUrl
+  module JetFuel
+    def initialize(protocol_and_host)
+      @protocol_and_host = protocol_and_host
+    end
 
-  def shorten(url, length=5)
-    digest = Digest::SHA256.hexdigest(url)[0..length]
-    @protocol_and_host + "/" + digest
+    def shorten(url, length=5)
+      digest = Digest::SHA256.hexdigest(url)[0..length]
+      @protocol_and_host + "/" + digest
+    end
   end
 end
